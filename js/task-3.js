0250ee13ddbc6b9d3c4e4f5816d44f35269cf988
HelenYakovenko <elenus.light@gmail.com>
2024-02-03 15:57:09 +0200

    hw

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,13 @@
-function getElementWidth(content, padding, border) {
-const isContentNumber = Number.parseFloat(content);
-const isPaddingNumber = Number.parseFloat(padding);
-const isBorderNumber = Number.parseFloat(border);
-const totalWidth = isContentNumber + 2 * isPaddingNumber + 2 * isBorderNumber;
-
-    return totalWidth;
+function checkForSpam(message) {
+    const lowerMassage = message.toLowerCase();
+    const  checkSpam = lowerMassage.includes("spam") || lowerMassage.includes("sale");
+    return checkSpam;
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); 
-console.log(getElementWidth("60px", "12px", "8.5px")); 
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
